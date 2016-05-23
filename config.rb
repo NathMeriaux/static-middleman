6a46afcffeed2d4ff0e954fb5894c2b288c11a84
Nath Meriaux <nath.meriaux@gmail.com>
2016-05-23 20:36:33 +0200

    Exo3 Middleman non-complet

--- config.rb
+++ config.rb
@@ -10,6 +10,9 @@
 # Auto-prefixing of CSS code with vendor prefix
 activate :autoprefixer
 
+["seb", "romain", "anne", "agnes"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
+end
 ###
 # Page options, layouts, aliases and proxies
 ###
